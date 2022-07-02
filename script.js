5c8a2133fed9df989ead23663231a0b0ac8345b2
Irakli <iraklimate@gmail.com>
2022-07-02 20:14:29 +0400

    get started button js correct complete

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let btn = document.getElementById('landingPageButton');
 
 btn.onclick = () => {
-  window.location.href = 'chess_experience/index.html'
+  window.location.href = 'account_registration/index.html'
 }
