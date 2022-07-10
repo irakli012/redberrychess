364b45f3e0c696b8b1ad1693de025e338e9a818c
Irakli_012 <iraklimate@gmail.com>
2022-07-10 16:15:48 +0400

    Add chess experience page styles

--- chess_experience/script.js
+++ chess_experience/script.js
@@ -7,12 +7,23 @@ backBtn.onclick = () => {
 
 // Next btn
 let nextBtn = document.getElementById('nextbtn-completepage');
+let answer = '';
+let answer1 = document.getElementById('yes');
+let answer2 = document.getElementById('no');
+
 
 nextBtn.onclick = () => {
-  localStorage.removeItem('name')
-  localStorage.removeItem('email')
-  localStorage.removeItem('phone')
-  localStorage.removeItem('birth')
+  if (answer1.checked) {
+    answer = answer1.value
+  }
+  else if (answer2.checked) {
+    answer = answer2.value
+  }
+  console.log(answer);
+  // localStorage.removeItem('name')
+  // localStorage.removeItem('email')
+  // localStorage.removeItem('phone')
+  // localStorage.removeItem('birth')
 
-  location.replace('/complete_page');
+  // location.replace('/complete_page');
 };
